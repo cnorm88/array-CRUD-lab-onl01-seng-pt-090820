eb9eee3acf8fae2d10420f9c97376bc37c97dc9f
Crystal Norman <cnorm88@gmail.com>
2020-09-15 16:10:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,8 +2,9 @@ def create_an_empty_array
   []
 end
 
-def create_an_array(array = [one, deux, trois, quarte])
-  puts create_an_array
+def create_an_array
+  array = [one, deux, trois, quarte]
+  
 end
 
 def add_element_to_end_of_array(array, element)
