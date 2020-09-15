009dd5b0002c302e1468247fa4c6ca7f1c44c274
Crystal Norman <cnorm88@gmail.com>
2020-09-15 17:45:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,8 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["un", "deux", "am"]
-  
+  index_number = ([2])
+  array.index
 end
 
 def retrieve_first_element_from_array(array)
