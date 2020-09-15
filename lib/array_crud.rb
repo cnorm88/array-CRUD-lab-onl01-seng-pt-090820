134b58717b044cc29ece29cdf7a3bc258a2be90c
Crystal Norman <cnorm88@gmail.com>
2020-09-15 17:30:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,9 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["un", "deux", "trois", "quatre"]
-  array << "cinq"
+  element = "cinq"
+  array << element
+  
 end
 
 def add_element_to_start_of_array(array, element)
